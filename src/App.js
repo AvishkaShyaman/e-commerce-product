c16a58f45eec06127da6bf88b7e77d4eef4c290b
Avishka Shyaman <avishkashyaman@gmail.com>
2022-05-17 20:53:11 +0530

    fixed style issues

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import {useEffect, useState} from "react";
-import {Button, Card} from "react-bootstrap";
+import { useEffect, useState } from 'react';
+import { Button, Card, Container, Row } from 'react-bootstrap';
 
 function App() {
-
-  const [product, setProduct] = useState({})
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products/1')
-        .then(res=>res.json())
-        .then(products=>setProduct(products))
+      .then((res) => res.json())
+      .then((products) => setProduct(products));
   }, []);
 
-
   return (
-      <div className="App">
-        {/* <h1 className="p-5 text-center">{product.title}</h1> */}
-        <div className="d-flex">
-          {
-
-                <Card className="m-2" style={{ width: '18rem' }} key={product.id}>
-                  <Card.Img variant="top" src={product.image} />
-                  <Card.Body>
-                    <Card.Title>{product.title}</Card.Title>
-                    <Card.Text>
-                      {product.description}
-                    </Card.Text>
-                    <Button variant="primary">Buy {product.price}</Button>
-                  </Card.Body>
-                </Card>
-          }
-          <div className="badge bg-success">
-              { product.rating && product.rating.rate}
-          </div>
-        </div>
-      </div>
+    <div className="App">
+      <h1 className="p-5 text-center">{product.title}</h1>
+      <Container className="">
+        <Row className="justify-content-md-center">
+          <Card
+            className="m-2 custom-card"
+            style={{ width: '30rem' }}
+            key={product.id}
+          >
+            <Row className="justify-content-md-center">
+              <Card.Img
+                style={{ width: '12rem' }}
+                variant="top"
+                src={product.image}
+              />
+            </Row>
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>{product.description}</Card.Text>
+              <div className="my-2">
+                <label className="m-1">Rating</label>
+                <div className="badge bg-success">
+                  {product.rating && product.rating.rate}
+                </div>
+              </div>
+              <Button variant="primary">Buy {product.price}</Button>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
